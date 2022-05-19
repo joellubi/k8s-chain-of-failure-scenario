5c1d4f14d9beb80884ba276d02a1a4a1c32994ec
joel <joellubi@gmail.com>
2022-05-19 07:03:36 -0400

    Add load-generator app

---
+++ k8s-chain-of-failure/apps/load-generator/index.js
@@ -0,0 +1,34 @@
+const https = require('https');
+
+
+const REQUESTS_PER_MINUTE = process.env.REQUESTS_PER_MINUTE || 100
+const requestInterval = 60 / REQUESTS_PER_MINUTE * 1000
+
+const options = {
+  hostname: 'some-microservice',
+  port: 80,
+  method: 'GET',
+};
+
+
+function sendRequest() {
+    const req = https.request(options, res => {
+      console.log(`statusCode: ${res.statusCode}`);
+    
+      res.on('data', d => {
+        process.stdout.write(d);
+      });
+    });
+    
+    req.on('error', error => {
+      console.error(error);
+    });
+    
+    req.end();
+}
+
+setInterval(() => sendRequest(), requestInterval)
+
+
+
+
