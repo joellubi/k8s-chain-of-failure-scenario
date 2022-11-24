ceff0240622443311336b630c3eff4619ae79862
joel <joellubi@gmail.com>
2022-11-24 15:00:46 -0500

    Add backend discovery to load generator

--- k8s-chain-of-failure/apps/load-generator/index.js
+++ k8s-chain-of-failure/apps/load-generator/index.js
@@ -1,34 +1,67 @@
-const https = require('http');
+const http = require('http');
 
 
 const REQUESTS_PER_MINUTE = process.env.REQUESTS_PER_MINUTE || 100
 const requestInterval = 60 / REQUESTS_PER_MINUTE * 1000
 
+const NUM_BACKENDS_EXPECTED = process.env.NUM_BACKENDS_EXPECTED
+
 const options = {
-  hostname: 'some-microservice',
+  hostname: 'microservice',
   port: 80,
   method: 'GET',
 };
 
+const backends_seen = new Set()
+
+const sendBackendDiscoveryRequest = () => {
+    const path = '/discover'
+    const req = http.request({ ...options, path}, res => {
+        res.on('data', d => {
+            console.log(d.toString())
+            const responseObject = JSON.parse(d.toString())
+            const serverID = responseObject['serverID']
+            backends_seen.add(serverID)
+        })
+      })
+
+      req.on('error', error => {
+        const errObj = {
+            "clientError": {
+                "name": error.name,
+                "stack": error.stack,
+                "message": error.message,
+            },
+        }
+      console.error(JSON.stringify(errObj));
+    })
+
+      req.end()
+}
 
 function sendRequest() {
-    const req = https.request(options, res => {
-      console.log(`statusCode: ${res.statusCode}`);
-    
+    if (backends_seen.size < NUM_BACKENDS_EXPECTED) {
+        sendBackendDiscoveryRequest()
+        return
+    }
+    const req = http.request(options, res => {
       res.on('data', d => {
-        process.stdout.write(d);
-      });
-    });
+        console.log(d.toString())
+      })
+    })
     
     req.on('error', error => {
-      console.error(error);
-    });
+        const errObj = {
+            "clientError": {
+                "name": error.name,
+                "stack": error.stack,
+                "message": error.message,
+            },
+        }
+      console.error(JSON.stringify(errObj));
+    })
     
-    req.end();
+    req.end()
 }
 
 setInterval(() => sendRequest(), requestInterval)
-
-
-
-
